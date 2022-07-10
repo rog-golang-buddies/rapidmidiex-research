9b283658dcbc36f24f30d77618ef22e75e9fcb7f
topheruk <kristopherab@gmail.com>
2022-07-10 18:57:32 +0000

    added enum

--- examples/ws-noughts-crosses/ws.go
+++ examples/ws-noughts-crosses/ws.go
@@ -6,13 +6,35 @@ type contextKey struct {
 
 func (k *contextKey) String() string { return "net/http context value " + k.name }
 
+type MessageType int
+
+const (
+	Connected MessageType = iota
+	Disconnected
+	Join
+	Leave
+	Play
+	// Unknown
+)
+
+func (m MessageType) String() string {
+	switch m {
+	case Connected:
+		return "connected"
+	case Disconnected:
+		return "disconnected"
+	case Join:
+		return "join"
+	case Leave:
+		return "leave"
+	case Play:
+		return "play"
+	default:
+		return "unknown"
+	}
+}
+
 type Message struct {
 	Type string `json:"type"`
 	Data any    `json:"data"`
-}
-
-type Play struct {
-	Turn  int
-	Pos   int
-	Moves int
 }
