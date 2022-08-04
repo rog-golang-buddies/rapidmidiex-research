04e572b0271eeb430ec96f4336e8949441e20079
hansvb <hans.vanbroeckhoven@gmail.com>
2022-08-04 15:46:14 +0200

    noughtscrosses: improve tests

--- examples/ws-noughts-crosses/message_test.go
+++ examples/ws-noughts-crosses/message_test.go
@@ -7,21 +7,34 @@ import (
 
 // test encoding for messageType
 func TestMessageTypeMarshalJSON(t *testing.T) {
-	var m MessageType
-	m = Connected
-	b, err := json.Marshal(m)
-	if err != nil {
-		t.Error(err)
+	tests := []struct {
+		m    MessageType
+		want string
+	}{
+		{Connected, `"connected"`},
+		{Disconnected, `"disconnected"`},
+		{Join, `"join"`},
+		{Leave, `"leave"`},
+		{Play, `"play"`},
+		{Reset, `"reset"`},
+		{Unknown, `"unknown"`},
 	}
-	if string(b) != "\"connected\"" {
-		t.Errorf("expected \"connected\", got %s", string(b))
+
+	for _, test := range tests {
+		b, err := json.Marshal(test.m)
+		if err != nil {
+			t.Error(err)
+		}
+		if string(b) != test.want {
+			t.Errorf("\nexpected: [%s]\ngot:      [%s]", test.want, string(b))
+		}
 	}
 }
 
 // test decoding for messageType
 func TestMessageTypeUnmarshalJSON(t *testing.T) {
 	var m MessageType
-	b := []byte("\"disconnected\"")
+	b := []byte(`"disconnected"`)
 	err := json.Unmarshal(b, &m)
 	if err != nil {
 		t.Error(err)
@@ -38,11 +51,14 @@ func TestMessageMarshalJSON(t *testing.T) {
 	m.Data = struct {
 		Id string `json:"id"`
 	}{Id: "123"}
+
+	expected := `{"type":"connected","data":{"id":"123"}}`
+
 	b, err := json.Marshal(m)
 	if err != nil {
 		t.Error(err)
 	}
-	if string(b) != "{\"type\":\"connected\",\"data\":{\"id\":\"123\"}}" {
-		t.Errorf("expected {\"type\":\"connected\",\"data\":{\"id\":\"123\"}}, got %s", string(b))
+	if string(b) != expected {
+		t.Errorf("\nexpected: [%s]\ngot:      [%s]", expected, string(b))
 	}
 }
